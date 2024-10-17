b3b0ac340c3fdfd078acb8d94a3066834c058aae
Matthew Schofield <matthew.schofield-5@student.manchester.ac.uk>
2024-10-17 19:41:04 +0100

    Added light and dark colour palletes to Tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "rgba(var(--background))",
+        txt: "rgba(var(--txt))",
+        button: "rgba(var(--button))",
+        "button-hover": "rgba(var(--button-hover))",
+        "button-text": "rgba(var(--button-text))",
+        card: "rgba(var(--card))",
+        "card-text": "rgba(var(--card-text))",
+        shad: "rgba(var(--shad))",
+        bingus: "rgba(var(--bingus))"
+      }
+    },
   },
   plugins: [],
 };
