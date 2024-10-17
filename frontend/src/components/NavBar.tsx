2146712664bd3fb0760a1159a5304b8cb630f727
Matthew Schofield <matthew.schofield-5@student.manchester.ac.uk>
2024-10-17 19:53:43 +0100

    Replaced explicit colour values in className fields with references to theme variables

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -3,14 +3,10 @@ import ToggleSlider from './ToggleSlider'
 
 export default function NavBar(){
     return(
-      <nav className="flex justify-center gap-5 m-5">
-        <NavLink className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" to={'/'}>All Entries</NavLink>
-        <NavLink className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" to={'/create'}>New Entry</NavLink>
-        
-        <div className='flex flex-col justify-center gap-1'>
-          <span className=' text-slate-400 text-md font-light'>Dark Mode: </span>
-          <ToggleSlider></ToggleSlider>
-        </div>
+      <nav className="flex justify-center h-20 align-content-center gap-5 m-5">
+        <NavLink className="m-3 p-4 text-xl bg-button hover:bg-button-hover rounded-md font-medium text-button-text" to={'/'}>All Entries</NavLink>
+        <NavLink className="m-3 p-4 text-xl bg-button hover:bg-button-hover rounded-md font-medium text-button-text" to={'/create'}>New Entry</NavLink>
+        <ToggleSlider></ToggleSlider>
       </nav>
     )
 }
