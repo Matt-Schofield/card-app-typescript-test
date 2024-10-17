a4d0216d346bfd055932237695a26331ce2b532c
Matthew Schofield <matthew.schofield-5@student.manchester.ac.uk>
2024-10-17 20:03:07 +0100

    Added global style properties and initial app theme via a useEffect hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from './components/NavBar'
 import AllEntries from './routes/AllEntries'
 import NewEntry from './routes/NewEntry'
@@ -11,22 +11,27 @@ import {
 } from "react-router-dom";
 
 export default function App() {
+  document.body.className = "bg-background";
+
+  useEffect(() => {
+    document.body.classList.add("lightTheme")
+  }, []);
+
   return (
     <section>
-  <Router>
-    <EntryProvider>
-    <NavBar></NavBar>
-      <Routes>
-        <Route path="/" element={<AllEntries/>}>
-        </Route>
-        <Route path="create" element={<NewEntry/>}>
-        </Route>
-        <Route path="edit/:id" element={<EditEntry/>}>
-        </Route>
-      </Routes>
-    </EntryProvider>
-    </Router>
+      <Router>
+        <EntryProvider>
+        <NavBar></NavBar>
+          <Routes>
+            <Route path="/" element={<AllEntries/>}>
+            </Route>
+            <Route path="create" element={<NewEntry/>}>
+            </Route>
+            <Route path="edit/:id" element={<EditEntry/>}>
+            </Route>
+          </Routes>
+        </EntryProvider>
+      </Router>
     </section>
-    
   );
 }
