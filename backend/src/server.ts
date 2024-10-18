8f9b1c89ea79141c61f66eee110bc1710576e88a
Matthew Schofield <matthew.schofield-5@student.manchester.ac.uk>
2024-10-18 18:06:27 +0100

    Applied husky formatting

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
-import fastify from "fastify";
 import cors from "@fastify/cors";
 import { Entry } from "@prisma/client";
+import fastify from "fastify";
 import Prisma from "./db";
 
 export const server = fastify();
@@ -12,18 +12,15 @@ server.get<{ Reply: Entry[] }>("/get/", async (req, reply) => {
   reply.send(dbAllEntries);
 });
 
-server.get<{ Body: Entry; Params: { id: string } }>(
-  "/get/:id",
-  async (req, reply) => {
-    const dbEntry = await Prisma.entry.findUnique({
-      where: { id: req.params.id },
-    });
-    if (!dbEntry) {
-      reply.status(500).send({ msg: `Error finding entry with id ${req.params.id}` });
-    }
-    reply.send(dbEntry);
+server.get<{ Body: Entry; Params: { id: string } }>("/get/:id", async (req, reply) => {
+  const dbEntry = await Prisma.entry.findUnique({
+    where: { id: req.params.id },
+  });
+  if (!dbEntry) {
+    reply.status(500).send({ msg: `Error finding entry with id ${req.params.id}` });
   }
-);
+  reply.send(dbEntry);
+});
 
 server.post<{ Body: Entry }>("/create/", async (req, reply) => {
   let newEntryBody = req.body;
@@ -31,9 +28,7 @@ server.post<{ Body: Entry }>("/create/", async (req, reply) => {
     ? (newEntryBody.created_at = new Date(req.body.created_at))
     : (newEntryBody.created_at = new Date());
 
-    newEntryBody.time
-    ? (newEntryBody.time = new Date(req.body.time))
-    : (newEntryBody.time = new Date());
+  newEntryBody.time ? (newEntryBody.time = new Date(req.body.time)) : (newEntryBody.time = new Date());
 
   try {
     const createdEntryData = await Prisma.entry.create({ data: req.body });
@@ -52,28 +47,21 @@ server.delete<{ Params: { id: string } }>("/delete/:id", async (req, reply) => {
   }
 });
 
-server.put<{ Params: { id: string }; Body: Entry }>(
-  "/update/:id",
-  async (req, reply) => {
-    let updatedEntryBody = req.body;
-    updatedEntryBody.created_at
-      ? (updatedEntryBody.created_at = new Date(req.body.created_at))
-      : (updatedEntryBody.created_at = new Date());
+server.put<{ Params: { id: string }; Body: Entry }>("/update/:id", async (req, reply) => {
+  let updatedEntryBody = req.body;
+  updatedEntryBody.created_at
+    ? (updatedEntryBody.created_at = new Date(req.body.created_at))
+    : (updatedEntryBody.created_at = new Date());
 
-    updatedEntryBody.time
-      ? (updatedEntryBody.time = new Date(req.body.time))
-      : (updatedEntryBody.time = new Date());
-      
-    try {
-      await Prisma.entry.update({
-        data: req.body,
-        where: { id: req.params.id },
-      });
-      reply.send({ msg: "Updated successfully" });
-    } catch {
-      reply.status(500).send({ msg: "Error updating" });
-    }
+  updatedEntryBody.time ? (updatedEntryBody.time = new Date(req.body.time)) : (updatedEntryBody.time = new Date());
+
+  try {
+    await Prisma.entry.update({
+      data: req.body,
+      where: { id: req.params.id },
+    });
+    reply.send({ msg: "Updated successfully" });
+  } catch {
+    reply.status(500).send({ msg: "Error updating" });
   }
-);
-
-
+});
